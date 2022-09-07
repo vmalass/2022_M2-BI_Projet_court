235ab46ee295059b3633fa020aae48e9fa69bd82
victor <victor.malassigne@inserm.fr>
2022-09-07 19:43:58 +0200

    remplace maths par numpy gain temps

--- bin/sphere.py
+++ bin/sphere.py
@@ -1,21 +1,23 @@
 import math
 import plotly.express as px
+import pandas
+import numpy
 
 
-def fibonacci_sphere(samples=100):
+def fibonacci_sphere(n=92):
 
     points = []
-    phi = math.pi * (3. - math.sqrt(5.))  # nombre d'or
+    phi = numpy.pi * (3. - numpy.sqrt(5.))  # nombre d'or
     sx =[]
     sy =[]
     sz =[]
-    for i in range(samples):
-        y = 1 - (i / float(samples - 1)) * 2
+    for i in range(n):
+        y = 1 - (i / float(n - 1)) * 2
 
-        radius = math.sqrt(1 - y * y)  # radius at y
+        radius = numpy.sqrt(1 - y * y)  # radius at y
         theta = phi * i  # golden angle increment
-        x = math.cos(theta) * radius
-        z = math.sin(theta) * radius
+        x = numpy.cos(theta) * radius
+        z = numpy.sin(theta) * radius
         rayon=1.7
         sx.append(rayon * x + 32.465)
         sy.append(rayon * y + 55.196)
